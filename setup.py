af4bd762ad61ec74d1f440f769634520a9e621ef
Ethan Rich <ethan.rich@neurocaregroup.com>
2022-08-16 13:46:54 +0200

    Update name

--- setup.py
+++ setup.py
@@ -14,18 +14,18 @@ with (Path(__file__).parent / "requirements.txt").open("r") as f:
 packages = setuptools.find_namespace_packages(exclude=["tests*", "docs*", "htmlcov*"])
 
 setup(
-    name="readEEG",
+    name="neuropraxpy",
     version="0.1.0",
     description="read binary EEG files into usable format for analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Ethan Rich",
     author_email="ethan.rich@neurocaregroup.com",
-    url="https://github.com/ethanrich/tool-readEEG.git",
-    download_url="https://github.com/ethanrich/tool-readEEG.git",
+    url="https://github.com/ethanrich/tool-neuropraxpy.git",
+    download_url="https://github.com/ethanrich/tool-neuropraxpy.git",
     license="MIT",
     packages=find_packages(),
-    entry_points={'console_scripts': ['readeeg=cli.main:main']},
+    entry_points={'console_scripts': ['neuropraxpy=cli.main:main']},
     install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
